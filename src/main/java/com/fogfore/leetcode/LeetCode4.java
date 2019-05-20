4755a1023aabf8ef9e62fab55c1dbf40794f2eba
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-20 17:19:49 +0800

    Create LeetCode4.java

---
+++ src/main/java/com/fogfore/leetcode/LeetCode4.java
@@ -0,0 +1,60 @@
+package com.fogfore.leetcode;
+
+public class LeetCode4 {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        boolean oddFlag = ((nums1.length + nums2.length) & 1) == 1;
+        int mid = (nums1.length + nums2.length) / 2;
+        int curIndex = 0;
+        int curNum = 0, preNum = 0;
+        boolean preNumPresent = false;
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                curNum = nums1[i];
+                i++;
+            } else {
+                curNum = nums2[j];
+                j++;
+            }
+            curIndex++;
+            if (curIndex == mid) {
+                preNum = curNum;
+            } else if (curIndex > mid) {
+                break;
+            }
+        }
+        while (i < nums1.length) {
+            if (curIndex == mid) {
+                preNum = curNum;
+            } else if (curIndex > mid) {
+                break;
+            }
+            curNum = nums1[i];
+            curIndex++;
+            i++;
+        }
+
+        while (j < nums2.length) {
+            if (curIndex == mid) {
+                preNum = curNum;
+            } else if (curIndex > mid) {
+                break;
+            }
+            curNum = nums2[j];
+            curIndex++;
+            j++;
+        }
+        if (oddFlag) {
+            return curNum;
+        } else {
+            return (curNum + preNum) / 2.0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3};
+        int[] b = {2};
+        System.out.println(findMedianSortedArrays(a, b));
+    }
+}
