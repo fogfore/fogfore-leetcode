66e7ad7ed5917ad5301380c887d926f4c0507d84
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-24 14:17:55 +0800

    最长公共前缀
    最长公共前缀

---
+++ src/main/java/com/fogfore/leetcode/LeetCode14.java
@@ -0,0 +1,29 @@
+package com.fogfore.leetcode;
+
+/**
+ * 最长公共前缀
+ *
+ * @author fogfore
+ */
+public class LeetCode14 {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length < 1) {
+            return "";
+        }
+        String common = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            for (int j = 0; j < common.length(); j++) {
+                if (j >= strs[i].length() || common.charAt(j) != strs[i].charAt(j)) {
+                    common = common.substring(0, j);
+                    break;
+                }
+            }
+        }
+        return common;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(" " + longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        System.out.println("fl " + longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+}
