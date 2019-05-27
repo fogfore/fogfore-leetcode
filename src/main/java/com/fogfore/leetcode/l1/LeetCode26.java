c210a54022ee114bcd6c021a70578024ed1a38a4
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-27 11:21:22 +0800

    删除排序数组中的重复项
    删除排序数组中的重复项

---
+++ src/main/java/com/fogfore/leetcode/l1/LeetCode26.java
@@ -0,0 +1,29 @@
+package com.fogfore.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 删除排序数组中的重复项
+ *
+ * @author fogfore
+ */
+public class LeetCode26 {
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int p1 = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[p1]) {
+                nums[++p1] = nums[i];
+            }
+        }
+        return p1 + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(array1));
+        System.out.println(Arrays.toString(array1));
+    }
+}
