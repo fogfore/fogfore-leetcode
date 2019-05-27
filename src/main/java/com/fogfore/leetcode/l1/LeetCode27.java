d42ae01215c214b23078f3c23ca4037468b8319a
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-27 11:33:54 +0800

    移除元素
    移除元素

---
+++ src/main/java/com/fogfore/leetcode/l1/LeetCode27.java
@@ -0,0 +1,29 @@
+package com.fogfore.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 移除元素
+ *
+ * @author fogfore
+ */
+public class LeetCode27 {
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[p++] = nums[i];
+            }
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(removeElement(array1, 2));
+        System.out.println(Arrays.toString(array1));
+    }
+}
