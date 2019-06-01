dc0ba730b69bb9a343b8087dd0b82f092074e833
fogfore <yh.924@foxmail.com>
2019-06-01 17:44:37 +0800

    组合总和

--- src/main/java/com/fogfore/leetcode/l1/LeetCode39.java
+++ src/main/java/com/fogfore/leetcode/l1/LeetCode39.java
@@ -8,33 +8,37 @@ import java.util.*;
  * @author fogfore
  */
 public class LeetCode39 {
+    // 回溯法
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-
-        return null;
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        LinkedList<Integer> queue = new LinkedList<>();
+        cal(candidates, target, 0, 0, queue, res);
+        return res;
     }
 
     private void cal(int[] candidates, int target, int cur, int sum, LinkedList<Integer> queue, List<List<Integer>> res) {
         if (cur >= candidates.length) {
             return;
         }
-        sum += candidates[cur];
-        queue.add(candidates[cur]);
         while (sum < target) {
             cal(candidates, target, cur + 1, sum, queue, res);
             sum += candidates[cur];
-            queue.add(candidates[cur]);
+            queue.addFirst(candidates[cur]);
         }
         if (sum == target) {
             res.add(new LinkedList<>(queue));
         }
-        while (!queue.isEmpty() && queue.peek() == candidates[cur]) {
-            queue.remove();
+        while (!queue.isEmpty() && queue.peekFirst() == candidates[cur]) {
+            queue.removeFirst();
         }
     }
 
+    // 动态规划 完全背包问题
+
     public static void main(String[] args) {
         LeetCode39 leetCode39 = new LeetCode39();
+        System.out.println(leetCode39.combinationSum(new int[]{2, 3, 5}, 7));
         System.out.println(leetCode39.combinationSum(new int[]{2, 3, 6, 7}, 7));
+        System.out.println(leetCode39.combinationSum(new int[]{2, 3, 5}, 8));
     }
 }
