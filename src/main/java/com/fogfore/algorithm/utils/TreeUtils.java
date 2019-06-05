dbb5e5a2d40dd4c58d56505afb4bb81ac084ed80
fogfore <38969741+fogfore@users.noreply.github.com>
2019-06-05 16:32:51 +0800

    Update TreeUtils.java

--- src/main/java/com/fogfore/algorithm/utils/TreeUtils.java
+++ src/main/java/com/fogfore/algorithm/utils/TreeUtils.java
@@ -1,49 +1,29 @@
 package com.fogfore.algorithm.utils;
 
 
-import com.sun.source.tree.Tree;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.List;
 import java.util.StringJoiner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class TreeUtils {
     private static final ThreadLocal<Integer> counter = new ThreadLocal<>();
 
-    /**
-     * 中序遍历，转换为字符串
-     *
-     * @param root 根节点
-     * @return
-     */
     public static String toStringByInOrder(TreeNode root) {
         StringJoiner joiner = new StringJoiner(",", "[", "]");
         toStringByInOrder(root, joiner);
         return joiner.toString();
     }
 
-    /**
-     * 中序遍历填充
-     *
-     * @param root   根节点
-     * @param values 集合
-     */
     public static void fillByInOrder(TreeNode root, List<Integer> values) {
         if (ObjectUtils.isEmpty(root)) {
             return;
         }
         fillByInOrder(root.left, values);
-        root.value = values.remove(0);
+        root.val = values.remove(0);
         fillByInOrder(root.right, values);
     }
 
-    /**
-     * 获取树的深度
-     *
-     * @param root 根节点
-     * @return
-     */
     public static int getDepth(TreeNode root) {
         if (ObjectUtils.isEmpty(root)) {
             return 0;
@@ -53,12 +33,6 @@ public class TreeUtils {
         return leftDepth > rightDepth ? rightDepth + 1 : rightDepth + 1;
     }
 
-    /**
-     * 是否平衡二叉树
-     *
-     * @param root 根节点
-     * @return
-     */
     public static boolean isBalanced(TreeNode root) {
         return isBalanced(root, new int[1]);
     }
@@ -81,11 +55,11 @@ public class TreeUtils {
     }
 
     private static void toStringByInOrder(TreeNode root, StringJoiner joiner) {
-        if (root == null) {
+        if (ObjectUtils.isEmpty(root)) {
             return;
         }
         toStringByInOrder(root.left, joiner);
-        joiner.add(String.valueOf(root.value));
+        joiner.add(String.valueOf(root.val));
         toStringByInOrder(root.right, joiner);
     }
 }
