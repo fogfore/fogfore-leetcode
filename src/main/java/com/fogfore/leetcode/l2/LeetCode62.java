a7e378657f555fa02f40a5a8d821442ed5ad9ac5
fogfore <38969741+fogfore@users.noreply.github.com>
2019-06-06 11:44:25 +0800

    不同路径
    不同路径

--- src/main/java/com/fogfore/leetcode/l2/LeetCode62.java
+++ src/main/java/com/fogfore/leetcode/l2/LeetCode62.java
@@ -8,17 +8,17 @@ package com.fogfore.leetcode.l2;
 public class LeetCode62 {
     public int uniquePaths(int m, int n) {
         if (m < 0 || n < 0) return 0;
-        int[][] dp = new int[2][m];
+        int[] dp = new int[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (i == 0 || j == 0) {
-                    dp[i & 1][j] = 1;
+                    dp[j] = 1;
                 } else {
-                    dp[i & 1][j] = dp[(i + 1) & 1][j] + dp[i & 1][j - 1];
+                    dp[j] = dp[j] + dp[j - 1];
                 }
             }
         }
-        return dp[(n + 1) & 1][m - 1];
+        return dp[m - 1];
     }
 
     public static void main(String[] args) {
