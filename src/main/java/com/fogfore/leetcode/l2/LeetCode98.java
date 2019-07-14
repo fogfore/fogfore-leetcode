b81d8100fd62b0131951a77e46ba4dcfead49479
fogfore <yh.924@foxmail.com>
2019-07-14 23:19:36 +0800

    二叉树的层次遍历

--- src/main/java/com/fogfore/leetcode/l2/LeetCode98.java
+++ src/main/java/com/fogfore/leetcode/l2/LeetCode98.java
@@ -11,18 +11,32 @@ import java.util.ArrayList;
  */
 public class LeetCode98 {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        ArrayList<Integer> list = new ArrayList<>();
-        return inOrder(root, list);
+        return isValidBST(root, null, null);
     }
 
-    private static boolean inOrder(TreeNode root, ArrayList<Integer> list) {
+    public boolean isValidBST(TreeNode root, Integer lower, Integer higher) {
         if (root == null) return true;
-        if (!inOrder(root.left, list)) return false;
-        if (!list.isEmpty() && root.val <= list.remove(0)) {
-            return false;
-        }
-        list.add(root.val);
-        return inOrder(root.right, list);
+        TreeNode left = root.left, right = root.right;
+        if (left != null && (left.val >= root.val || (lower != null && left.val <= lower))) return false;
+        if (right != null && (right.val <= root.val || (higher != null && right.val >= higher))) return false;
+
+        return isValidBST(left, lower, root.val) && isValidBST(right, root.val, higher);
     }
+
+    // 中序遍历
+//    public boolean isValidBST(TreeNode root) {
+//        if (root == null) return true;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        return inOrder(root, list);
+//    }
+//
+//    private static boolean inOrder(TreeNode root, ArrayList<Integer> list) {
+//        if (root == null) return true;
+//        if (!inOrder(root.left, list)) return false;
+//        if (!list.isEmpty() && root.val <= list.remove(0)) {
+//            return false;
+//        }
+//        list.add(root.val);
+//        return inOrder(root.right, list);
+//    }
 }
